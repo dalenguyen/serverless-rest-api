9661525612557e79765e6554529c7896d1fedf93
Dale Nguyen <dungnq@itbox4vn.com>
2019-08-03 15:05:55 -0400

    Updated serverless project

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,6 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
-import * as firebaseHelper from 'firebase-functions-helper';
+import * as firebaseHelper from 'firebase-functions-helper/dist';
 import * as express from 'express';
 import * as bodyParser from "body-parser";
 
@@ -18,26 +18,42 @@ const contactsCollection = 'contacts';
 
 export const webApi = functions.https.onRequest(main);
 
+interface Contact {
+    firstName: String
+    lastName: String
+    email: String
+}
 
 // Add new contact
-app.post('/contacts', (req, res) => {
-    firebaseHelper.firestore
-        .creatNewDocument(db, contactsCollection, req.body);
-    res.send('Create a new contact');
+app.post('/contacts', async (req, res) => {
+    try {
+        const contact: Contact = {
+            firstName: req.body['firstName'],
+            lastName: req.body['lastName'],
+            email: req.body['email']
+        }
+
+        const newDoc = await firebaseHelper.firestore
+            .createNewDocument(db, contactsCollection, contact);
+        res.status(201).send(`Created a new contact: ${newDoc.id}`);
+    } catch (error) {
+        res.status(400).send(`Contact should only contains firstName, lastName and email!!!`)
+    }        
 })
 
 // Update new contact
-app.patch('/contacts/:contactId', (req, res) => {
-    firebaseHelper.firestore
+app.patch('/contacts/:contactId', async (req, res) => {
+    const updatedDoc = await firebaseHelper.firestore
         .updateDocument(db, contactsCollection, req.params.contactId, req.body);
-    res.send('Update a new contact');
+    res.status(204).send(`Update a new contact: ${updatedDoc}`);
 })
 
 // View a contact
 app.get('/contacts/:contactId', (req, res) => {
     firebaseHelper.firestore
         .getDocument(db, contactsCollection, req.params.contactId)
-        .then(doc => res.status(200).send(doc));
+        .then(doc => res.status(200).send(doc))
+        .catch(error => res.status(400).send(`Cannot get contact: ${error}`));
 })
 
 // View all contacts
@@ -45,11 +61,12 @@ app.get('/contacts', (req, res) => {
     firebaseHelper.firestore
         .backup(db, contactsCollection)
         .then(data => res.status(200).send(data))
+        .catch(error => res.status(400).send(`Cannot get contacts: ${error}`));
 })
 
 // Delete a contact 
-app.delete('/contacts/:contactId', (req, res) => {
-    firebaseHelper.firestore
+app.delete('/contacts/:contactId', async (req, res) => {
+    const deletedContact = await firebaseHelper.firestore
         .deleteDocument(db, contactsCollection, req.params.contactId);
-    res.send('Contact is deleted');
+    res.status(204).send(`Contact is deleted: ${deletedContact}`);
 })
